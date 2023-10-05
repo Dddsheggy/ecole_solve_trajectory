9e4d8b1c5ac18c7635aa20575fc7136c40cba3a4
Dddsheggy <dushuhan17@163.com>
2023-10-05 21:13:39 +0800

    code for sampling

---
+++ sampling.py
@@ -0,0 +1,87 @@
+import gzip
+import pickle
+from pathlib import Path
+import glob
+import ecole
+import numpy as np
+
+DATA_MAX_SAMPLES = 100000
+
+instances = glob.glob('data/*.mps')
+
+class ExploreThenStrongBranch:
+    """
+    This custom observation function class will randomly return either strong branching scores (expensive expert)
+    or pseudocost scores (weak expert for exploration) when called at every node.
+    """
+
+    def __init__(self, expert_probability):
+        self.expert_probability = expert_probability
+        self.pseudocosts_function = ecole.observation.Pseudocosts()
+        self.strong_branching_function = ecole.observation.StrongBranchingScores()
+
+    def before_reset(self, model):
+        """
+        This function will be called at initialization of the environment (before dynamics are reset).
+        """
+        self.pseudocosts_function.before_reset(model)
+        self.strong_branching_function.before_reset(model)
+
+    def extract(self, model, done):
+        """
+        Should we return strong branching or pseudocost scores at time node?
+        """
+        probabilities = [1 - self.expert_probability, self.expert_probability]
+        expert_chosen = bool(np.random.choice(np.arange(2), p=probabilities))
+        if expert_chosen:
+            return (self.strong_branching_function.extract(model, done), True)
+        else:
+            return (self.pseudocosts_function.extract(model, done), False)
+        
+# pass custom SCIP parameters
+scip_parameters = {
+    "separating/maxrounds": 0,
+    "presolving/maxrestarts": 0,
+    "limits/time": 3600,
+}
+
+# tuple observation functions to return complex state information
+env = ecole.environment.Branching(
+    observation_function=(
+        ExploreThenStrongBranch(expert_probability=0.1),
+        ecole.observation.NodeBipartite(),
+    ),
+    scip_params=scip_parameters,
+)
+
+# This will seed the environment for reproducibility
+env.seed(0)
+
+
+episode_counter, sample_counter = 0, 0
+sample_dir = "samples/"
+Path(sample_dir).mkdir(exist_ok=False)
+
+rng = np.random.RandomState(0)
+
+# solve problems (run episodes) until we have saved enough samples
+while sample_counter < DATA_MAX_SAMPLES:
+    episode_counter += 1
+    instance = rng.choice(instances)
+    observation, action_set, _, done, _ = env.reset(instance)
+    while not done:
+        (scores, scores_are_expert), node_observation = observation
+        action = action_set[scores[action_set].argmax()]
+
+        # Only save samples if they are coming from the expert (strong branching)
+        if scores_are_expert and (sample_counter < DATA_MAX_SAMPLES):
+            sample_counter += 1
+            data = [node_observation, action, action_set, scores]
+            filename = sample_dir + f"sample_{sample_counter}.pkl"
+
+            with gzip.open(filename, "wb") as f:
+                pickle.dump(data, f)
+
+        observation, action_set, _, done, _ = env.step(action)
+
+    print(f"Episode {episode_counter}, {sample_counter} samples collected so far")
