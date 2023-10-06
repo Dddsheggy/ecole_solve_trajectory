e2222619271c89338fd0988324d27aa78c07748c
Dddsheggy <dushuhan17@163.com>
2023-10-06 12:06:06 +0800

    fix the bug of nan in variable features

---
+++ training.py
@@ -0,0 +1,358 @@
+import gzip
+import pickle
+from pathlib import Path
+import glob
+
+import ecole
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch_geometric
+
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(DEVICE)
+
+
+class BipartiteNodeData(torch_geometric.data.Data):
+    """
+    This class encode a node bipartite graph observation as returned by the `ecole.observation.NodeBipartite`
+    observation function in a format understood by the pytorch geometric data handlers.
+    """
+
+    def __init__(
+        self,
+        constraint_features,
+        edge_indices,
+        edge_features,
+        variable_features,
+        candidates,
+        nb_candidates,
+        candidate_choice,
+        candidate_scores,
+    ):
+        super().__init__()
+        self.constraint_features = constraint_features
+        self.edge_index = edge_indices
+        self.edge_attr = edge_features
+        self.variable_features = variable_features
+        self.candidates = candidates
+        self.nb_candidates = nb_candidates
+        self.candidate_choices = candidate_choice
+        self.candidate_scores = candidate_scores
+
+    def __inc__(self, key, value, store, *args, **kwargs):
+        """
+        We overload the pytorch geometric method that tells how to increment indices when concatenating graphs
+        for those entries (edge index, candidates) for which this is not obvious.
+        """
+        if key == "edge_index":
+            return torch.tensor(
+                [[self.constraint_features.size(0)], [self.variable_features.size(0)]]
+            )
+        elif key == "candidates":
+            return self.variable_features.size(0)
+        else:
+            return super().__inc__(key, value, *args, **kwargs)
+
+
+class GraphDataset(torch_geometric.data.Dataset):
+    """
+    This class encodes a collection of graphs, as well as a method to load such graphs from the disk.
+    It can be used in turn by the data loaders provided by pytorch geometric.
+    """
+
+    def __init__(self, sample_files):
+        super().__init__(root=None, transform=None, pre_transform=None)
+        self.sample_files = sample_files
+
+    def len(self):
+        return len(self.sample_files)
+
+    def get(self, index):
+        """
+        This method loads a node bipartite graph observation as saved on the disk during data collection.
+        """
+        with gzip.open(self.sample_files[index], "rb") as f:
+            sample = pickle.load(f)
+
+        sample_observation, sample_action, sample_action_set, sample_scores = sample
+        
+        constraint_features = sample_observation.row_features
+        edge_indices = sample_observation.edge_features.indices.astype(np.int32)
+        edge_features = np.expand_dims(sample_observation.edge_features.values, axis=-1)
+        # variable_features = sample_observation.variable_features
+        variable_features = sample_observation.column_features
+
+        # We note on which variables we were allowed to branch, the scores as well as the choice
+        # taken by strong branching (relative to the candidates)
+        candidates = np.array(sample_action_set, dtype=np.int32)
+        candidate_scores = np.array([sample_scores[j] for j in candidates])
+        candidate_choice = np.where(candidates == sample_action)[0][0]
+
+        graph = BipartiteNodeData(
+            torch.FloatTensor(constraint_features),
+            torch.LongTensor(edge_indices),
+            torch.FloatTensor(edge_features),
+            torch.FloatTensor(variable_features),
+            torch.LongTensor(candidates),
+            len(candidates),
+            torch.LongTensor([candidate_choice]),
+            torch.FloatTensor(candidate_scores)
+        )
+
+        # We must tell pytorch geometric how many nodes there are, for indexing purposes
+        graph.num_nodes = constraint_features.shape[0] + variable_features.shape[0]
+
+        return graph
+
+
+sample_dir = "samples/"
+sample_files = [str(path) for path in Path(sample_dir).glob("sample_*.pkl")]
+train_files = sample_files[: int(0.8 * len(sample_files))]
+valid_files = sample_files[int(0.8 * len(sample_files)) :]
+
+train_data = GraphDataset(train_files)
+train_loader = torch_geometric.loader.DataLoader(train_data, batch_size=32, shuffle=True)
+valid_data = GraphDataset(valid_files)
+valid_loader = torch_geometric.loader.DataLoader(valid_data, batch_size=128, shuffle=False)
+
+
+class GNNPolicy(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        emb_size = 64
+        cons_nfeats = 5
+        edge_nfeats = 1
+        var_nfeats = 19
+
+        # CONSTRAINT EMBEDDING
+        self.cons_embedding = torch.nn.Sequential(
+            torch.nn.LayerNorm(cons_nfeats),
+            torch.nn.Linear(cons_nfeats, emb_size),
+            torch.nn.ReLU(),
+            torch.nn.Linear(emb_size, emb_size),
+            torch.nn.ReLU(),
+        )
+
+        # EDGE EMBEDDING
+        self.edge_embedding = torch.nn.Sequential(
+            torch.nn.LayerNorm(edge_nfeats),
+        )
+
+        # VARIABLE EMBEDDING
+        self.var_embedding = torch.nn.Sequential(
+            torch.nn.LayerNorm(var_nfeats),
+            torch.nn.Linear(var_nfeats, emb_size),
+            torch.nn.ReLU(),
+            torch.nn.Linear(emb_size, emb_size),
+            torch.nn.ReLU(),
+        )
+
+        self.conv_v_to_c = BipartiteGraphConvolution()
+        self.conv_c_to_v = BipartiteGraphConvolution()
+
+        self.output_module = torch.nn.Sequential(
+            torch.nn.Linear(emb_size, emb_size),
+            torch.nn.ReLU(),
+            torch.nn.Linear(emb_size, 1, bias=False),
+        )
+
+    def forward(
+        self, constraint_features, edge_indices, edge_features, variable_features
+    ):
+        reversed_edge_indices = torch.stack([edge_indices[1], edge_indices[0]], dim=0)
+
+        # First step: linear embedding layers to a common dimension (64)
+        constraint_features = self.cons_embedding(constraint_features)
+        edge_features = self.edge_embedding(edge_features)
+        variable_features = self.var_embedding(variable_features)
+
+        # Two half convolutions
+        constraint_features = self.conv_v_to_c(
+            variable_features, reversed_edge_indices, edge_features, constraint_features
+        )
+        variable_features = self.conv_c_to_v(
+            constraint_features, edge_indices, edge_features, variable_features
+        )
+
+        # A final MLP on the variable features
+        output = self.output_module(variable_features).squeeze(-1)
+        return output
+
+
+class BipartiteGraphConvolution(torch_geometric.nn.MessagePassing):
+    """
+    The bipartite graph convolution is already provided by pytorch geometric and we merely need
+    to provide the exact form of the messages being passed.
+    """
+
+    def __init__(self):
+        super().__init__("add")
+        emb_size = 64
+
+        self.feature_module_left = torch.nn.Sequential(
+            torch.nn.Linear(emb_size, emb_size)
+        )
+        self.feature_module_edge = torch.nn.Sequential(
+            torch.nn.Linear(1, emb_size, bias=False)
+        )
+        self.feature_module_right = torch.nn.Sequential(
+            torch.nn.Linear(emb_size, emb_size, bias=False)
+        )
+        self.feature_module_final = torch.nn.Sequential(
+            torch.nn.LayerNorm(emb_size),
+            torch.nn.ReLU(),
+            torch.nn.Linear(emb_size, emb_size),
+        )
+
+        self.post_conv_module = torch.nn.Sequential(torch.nn.LayerNorm(emb_size))
+
+        # output_layers
+        self.output_module = torch.nn.Sequential(
+            torch.nn.Linear(2 * emb_size, emb_size),
+            torch.nn.ReLU(),
+            torch.nn.Linear(emb_size, emb_size),
+        )
+
+    def forward(self, left_features, edge_indices, edge_features, right_features):
+        """
+        This method sends the messages, computed in the message method.
+        """
+        output = self.propagate(
+            edge_indices,
+            size=(left_features.shape[0], right_features.shape[0]),
+            node_features=(left_features, right_features),
+            edge_features=edge_features,
+        )
+        return self.output_module(
+            torch.cat([self.post_conv_module(output), right_features], dim=-1)
+        )
+
+    def message(self, node_features_i, node_features_j, edge_features):
+        output = self.feature_module_final(
+            self.feature_module_left(node_features_i)
+            + self.feature_module_edge(edge_features)
+            + self.feature_module_right(node_features_j)
+        )
+        return output
+
+
+policy = GNNPolicy().to(DEVICE)
+
+observation = train_data[0].to(DEVICE)
+observation.variable_features = torch.nan_to_num(observation.variable_features, nan=0.0)
+
+logits = policy(
+    observation.constraint_features,
+    observation.edge_index,
+    observation.edge_attr,
+    observation.variable_features,
+)
+action_distribution = F.softmax(logits[observation.candidates], dim=-1)
+
+print(action_distribution)
+
+
+def process(policy, data_loader, optimizer=None):
+    """
+    This function will process a whole epoch of training or validation, depending on whether an optimizer is provided.
+    """
+    mean_loss = 0
+    mean_acc = 0
+
+    n_samples_processed = 0
+    with torch.set_grad_enabled(optimizer is not None):
+        for batch in data_loader:
+            batch = batch.to(DEVICE)
+            # Compute the logits (i.e. pre-softmax activations) according to the policy on the concatenated graphs
+            logits = policy(
+                batch.constraint_features,
+                batch.edge_index,
+                batch.edge_attr,
+                batch.variable_features,
+            )
+            # Index the results by the candidates, and split and pad them
+            logits = pad_tensor(logits[batch.candidates], batch.nb_candidates)
+            # Compute the usual cross-entropy classification loss
+            loss = F.cross_entropy(logits, batch.candidate_choices)
+
+            if optimizer is not None:
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            true_scores = pad_tensor(batch.candidate_scores, batch.nb_candidates)
+            true_bestscore = true_scores.max(dim=-1, keepdims=True).values
+
+            predicted_bestindex = logits.max(dim=-1, keepdims=True).indices
+            accuracy = (
+                (true_scores.gather(-1, predicted_bestindex) == true_bestscore)
+                .float()
+                .mean()
+                .item()
+            )
+
+            mean_loss += loss.item() * batch.num_graphs
+            mean_acc += accuracy * batch.num_graphs
+            n_samples_processed += batch.num_graphs
+
+    mean_loss /= n_samples_processed
+    mean_acc /= n_samples_processed
+    return mean_loss, mean_acc
+
+
+def pad_tensor(input_, pad_sizes, pad_value=-1e8):
+    """
+    This utility function splits a tensor and pads each split to make them all the same size, then stacks them.
+    """
+    max_pad_size = pad_sizes.max()
+    output = input_.split(pad_sizes.cpu().numpy().tolist())
+    output = torch.stack(
+        [
+            F.pad(slice_, (0, max_pad_size - slice_.size(0)), "constant", pad_value)
+            for slice_ in output
+        ],
+        dim=0,
+    )
+    return output
+
+
+lr = 0.001
+NB_EPOCHS = 500
+
+patience = 15
+early_stopping = 30
+
+optimizer = torch.optim.Adam(policy.parameters(), lr=lr)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.2, patience=patience, verbose=True)
+best_loss = np.inf
+
+policy_name = "policies/35T_small_policy.pkl"
+
+for epoch in range(NB_EPOCHS):
+    print(f"Epoch {epoch+1}")
+
+    train_loss, train_acc = process(policy, train_loader, optimizer)
+    print(f"Train loss: {train_loss:0.3f}, accuracy {train_acc:0.3f}")
+
+    valid_loss, valid_acc = process(policy, valid_loader, None)
+    print(f"Valid loss: {valid_loss:0.3f}, accuracy {valid_acc:0.3f}")
+
+
+    if valid_loss < best_loss:
+        plateau_count = 0
+        best_loss = valid_loss
+        torch.save(policy, policy_name)
+        print("best model so far")
+    else:
+        plateau_count += 1
+        if plateau_count % early_stopping == 0:
+            print(f"{plateau_count} epochs without improvement, early stopping")
+            break
+        if plateau_count % patience == 0:
+            lr *= 0.2
+            print(f"{plateau_count} epochs without improvement, decreasing learning rate to {lr}")
+    scheduler.step(valid_loss)
+
+torch.save(policy, policy_name)
